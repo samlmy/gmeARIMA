98001f047f784a286236104d1aa789934adae976
Mingyang Li <mingyangsamli@gmail.com>
2022-04-27 18:56:03 -0400

    Added final version of report
    Added final version of report files

--- scripts/01-data_cleaning.R
+++ scripts/01-data_cleaning.R
@@ -1,36 +1,27 @@
 #### Preamble ####
-# Purpose: Clean the survey data downloaded from [...UPDATE ME!!!!!]
-# Author: Rohan Alexander [CHANGE THIS TO YOUR NAME!!!!]
-# Data: 3 January 2021
-# Contact: rohan.alexander@utoronto.ca [PROBABLY CHANGE THIS ALSO!!!!]
+# Purpose: Clean the data downloaded from tidyquant.
+# Author: Mingyang Li
+# Data: April 27 2022
+# Contact: wowsmyl.li@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: 
-# - Need to have downloaded the ACS data and saved it to inputs/data
-# - Don't forget to gitignore it!
-# - Change these to yours
-# Any other information needed?
-
 
 #### Workspace setup ####
-# Use R Projects, not setwd().
+library(tidyquant)
 library(haven)
 library(tidyverse)
-# Read in the raw data. 
-raw_data <- readr::read_csv("inputs/data/raw_data.csv"
-                     )
-# Just keep some variables that may be of interest (change 
-# this depending on your interests)
-names(raw_data)
 
-reduced_data <- 
-  raw_data %>% 
-  select(first_col, 
-         second_col)
-rm(raw_data)
+# get data from tidyquant
+
+options("getSymbols.warning4.0"=FALSE)
+options("getSymbols.yahoo.warning"=FALSE)
+
+gme <- getSymbols("GME", from = '2020-11-01',
+                  to = "2022-04-22", warnings = FALSE,
+                  auto.assign = FALSE)
          
 
-#### What's next? ####
-
+#### save data ####
+write.csv(gme, "inputs/data/gme.csv")
 
 
          
